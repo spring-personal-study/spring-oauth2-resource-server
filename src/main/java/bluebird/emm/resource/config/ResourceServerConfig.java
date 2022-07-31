aadda2a63243253fc7b24684dea3638c106f0d9a
chaenabi <find1086@gmail.com>
2022-07-31 16:46:55 +0900

    Web Security 구성

--- src/main/java/bluebird/emm/resource/config/ResourceServerConfig.java
+++ src/main/java/bluebird/emm/resource/config/ResourceServerConfig.java
@@ -5,9 +5,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
+    Web Security 구성
+ */
 @EnableWebSecurity
 public class ResourceServerConfig {
 
+    /*
+        article resource 에 대한 모든 요청은 승인이 요구되어야 하고
+        scope 로 적절한 권한 (articles.read)이 있어야 접근이 가능하도록 설정
+     */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.mvcMatcher("/articles/**")
